ebe563e5a021fac09bc88c01635a914728c8fe28
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-08-20 21:17:40 +0200

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./app.css";
+
+const App = () => {
+    return (
+        <div className="w-full h-screen flex justify-center items-center bg-black text-white text-4">
+            Hello
+        </div>
+    );
+};
+
+export default App;
