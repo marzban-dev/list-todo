8b8b22036dc3b961dac5939bd3a4b7d000ec6cdb
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-08-21 19:44:22 +0200

    Add spinner icon

---
+++ src/components/svg-components/spinner-third.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import CompSpinnerThird from "@/components/svg-components/svgs/spinner-third.svg"
+
+const IconSpinnerThird = (props : {className?:string; style?:React.CSSProperties}) => {
+    return <CompSpinnerThird {...props}/>
+}
+
+export default IconSpinnerThird
