cc254f6742c472a7f562270311cb485f710bc5f1
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-08-24 14:04:52 +0200

    Off retry

--- src/hooks/use-todo.tsx
+++ src/hooks/use-todo.tsx
@@ -2,5 +2,5 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchTodo } from "@/api/todo";
 
 export const useTodoQuery = (id: number) => {
-    return useQuery(["todo", id], () => fetchTodo(id));
+    return useQuery(["todo", id], () => fetchTodo(id), { retry: false });
 };
